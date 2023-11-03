6dd8c20c2d21f9af67dfcb64ddc3cd0f38b61dd9
D H <daniel.f.hyatt@gmail.com>
2023-11-03 15:18:42 -0600

    3:15 Friday end day

--- src/arrays.js
+++ src/arrays.js
@@ -19,13 +19,18 @@ function greaterThanTen(numbers) {
 // Ex.:
 //   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
 //   => ['banana', 'Bonobo]
-
 function bWords(words) {
-  const strings = [];
-  for (let string of strings) {
-
+  const bWord = [];
+  for (let i = 0; i < words.length; i++){
+    if (words[i][0].toLowerCase() === 'b') {
+      bWord.push(words[i]);
+    }
   }
+  return bWord ;
 }
+
+
+
 
 
 // Add all the elements from additionalItems to the end of originalArray.
@@ -34,8 +39,15 @@ function bWords(words) {
 //  extend([1, 2, 3], [4, 5, 6]);
 //  => [1, 2, 3, 4, 5, 6]
 function extend(originalArray, additionalItems) {
-  originalArray.push(additionalItems);
-  return originalArray;
+  finalArray = [];
+  for (let i = 0; i < originalArray.length; i++) {
+    finalArray.push(originalArray[i]);
+  }
+  for (let i = 0; i < additionalItems.length; i++) {
+    finalArray.push(additionalItems[i]);
+  }
+  return finalArray;
+
 }
 
 
@@ -44,16 +56,15 @@ function extend(originalArray, additionalItems) {
 // Ex.:
 //   itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
 //   => ['bbb', 'eee']
-function itemsWithLength(items, length) {}
-
-
-
-
-
-
-
-
-
+function itemsWithLength(items, length) {
+  finalArray = [];
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].length === length ) {
+      finalArray.push(items[i])
+    }
+  }
+  return finalArray;
+}
 
 
 
@@ -64,7 +75,21 @@ function itemsWithLength(items, length) {}
 // Ex.:
 //   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
 //   => ['a', 'c', 'e']
-function everyOtherItem(items) {}
+function everyOtherItem(items) {
+  finalArray = [];
+  for (let i = 0; i < items.length; i += 2) {
+    finalArray.push(items[i])
+  }
+  return finalArray;
+}
+
+
+
+
+
+
+
+
 
 // Given a list of words and a letter, return the indexes of the words that
 // start with that letter. You can assume that the words and letter will always
@@ -74,13 +99,56 @@ function everyOtherItem(items) {}
 //   => [1, 4]
 function findWordsStartingWith(words, letter) {}
 
+
+// Starting point
+let words = ['apple', 'banana', 'kiwi', 'pear', 'bacon'];
+let letter = 'b';
+
+function findWordsStartingWith(words, letter) {
+  let finalArray = [];
+  for (let i = 0; i < words.length; i++) {
+     finalArray.push(words[i][0]);
+    ...
+  }
+
+  return finalArray;
+}
+console.log(findWordsStartingWith(words, letter));
+// end of this problem
+
+
+
+
+
+
+
+
+
+
+
 // Return the `n` smallest values in the array in descending order (largest
 // numbers first). Assume that `n` will always be less than the length of the
 // array.
 // Ex.:
 //   smallestNItems([1, 30, 4, 21, 100000], 3);
 //   => [21, 4, 1]
-function smallestNItems(items, n) {}
+function smallestNItems(items, n) {
+  smallestValuesArray = [];
+  for (let i = 0; i < items.length; i++) {
+      smallestValuesArray.push(items[i]);
+  }
+  smallestValuesArray.sort(function(a, b){return a-b});
+  let Finalarray = smallestValuesArray.slice(0, n);
+  Finalarray.sort(function(a, b){return b-a});
+  
+return Finalarray;
+}
+
+
+
+
+
+
 
 // Search for a value in the array and return its index. If the value appears
 // more than once, return the index of the *FIRST* occurrence of the value. If
@@ -88,7 +156,24 @@ function smallestNItems(items, n) {}
 // Ex.:
 //   findIndex(['a', 'b', 'c', 'a', 'b', 'c'], 'c');
 //   => 2
-function findIndex(items, value) {}
+function findIndex(items, value) {
+  let NewArray = [];
+  for (let i = 0; i < items.length; i++) {
+      NewArray.push(items[i]);
+  }
+  let NewArray2 = NewArray.indexOf(value);
+    
+  return NewArray2;
+}
+
+
+
+
+
+
+
+
+
 
 // Given a start number and stop number, return a new array containing all the numbers
 // between the start and stop number.
@@ -96,6 +181,13 @@ function findIndex(items, value) {}
 //   range(1, 5);
 //   => [1, 2, 3, 4, 5]
 function range(start, stop) {}
+
+
+
+
+
+
+
 
 export {
   bWords,
--- src/functions.js
+++ src/functions.js
@@ -7,49 +7,93 @@ function divide(x, y) {
   return quotient;
 }
 
-
-
-
-
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
 // computed by dividing the sum of x and y by 2.
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
+function average(x, y) {
+  mean = (x + y)/2;
+  return mean;
+}
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
 // Ex.:
 //   approximatelyEqual(10.001, 10);
 //   => true
-function approximatelyEqual(x, y) {}
+function approximatelyEqual(x, y) {
+    (Math.abs(x - y) < .001 ) 
+}
 
 // Given a first name and last name, return a full name in the format "FIRST LAST"
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
+function fullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
 // Ex.:
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
-function generateSentence(person, beverage, location) {}
+function generateSentence(person, beverage, location) {
+  return person + " was drinking " + beverage + " at " + location + ".";
+}
+
+
 
 // Return the given string with all vowels replced with '*' characters.
 // Ex.:
 //   censorVowels('javascript');
 //   => 'j*v*scr*pt'
-function censorVowels(string) {}
+function censorVowels(string) {
+  let newstring = string
+  .replace("a" , "*")
+  .replace("a" , "*")
+  .replace("e" , "*")
+  .replace("o" , "*")
+  .replace("u" , "*")
+  .replace("i", "*");
+
+return newstring;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Return the given string in sticky case.
 // Ex.:
 //   stickyCase('hello world');
 //   => 'hElLo wOrLd'
-function stickyCase(string) {}
+function stickyCase(string) {
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 // Return the given string in leetspeak. Leetspeak is a modified version of
 // English where characters are replaced by numbers or symbols. For this
@@ -63,7 +107,11 @@ function stickyCase(string) {}
 // Ex.:
 //   leetspeak('javascript');
 //   => 'j4v45cr1p7'
-function leetspeak(string) {}
+function leetspeak(string) {
+
+
+  
+}
 
 export {
   approximatelyEqual,
